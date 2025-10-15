8f7167671196cd748de5d7aef7b7497091f9a58b
Neev Chordeker <neevch19@gmail.com>
2025-10-15 17:41:50 +0300

    add db mysql

---
+++ for_in_a_row/connect4_db.sql
@@ -0,0 +1,12 @@
+create DATABASE if not exists connect4;
+use connect4;
+
+create table if not exists games (
+	id int auto_increment primary key,
+    code varchar(100) unique,
+    board text,
+    turn int default 1,
+    winner int default null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp
+    );
